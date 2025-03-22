bf354d1a37f8122dee80ee048c12bfb445705d1d
taouamwalideddine <wtaouam@gmail.com>
2025-03-22 20:42:42 +0000

    setted auth

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PlantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::get('/plants', [PlantController::class, 'index']);
+Route::get('/plants/{slug}', [PlantController::class, 'show']);
